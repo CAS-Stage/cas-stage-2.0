2907a789611d6b3bc717767847eede404ba839d1
Jorge G. <jguitard@tie.cl>
2012-11-10 20:01:21 -0300

    Archivo DML
    Datos cargados en el proyecto hasta el momento. Importar con MySQL una
    vez creara la base de datos "cas-stage" a partir del DDL
    (cas-stage.sql).

---
+++ data.sql
@@ -0,0 +1,285 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-11-2012 a las 23:59:39
+-- Versión del servidor: 5.5.27-log
+-- Versión de PHP: 5.4.7
+
+SET FOREIGN_KEY_CHECKS=0;
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT=0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `cas-stage`
+--
+
+--
+-- Volcado de datos para la tabla `comuna`
+--
+
+INSERT INTO `comuna` (`id`, `nombre`) VALUES
+(1, 'Cerrillos'),
+(2, 'Cerro Navia'),
+(3, 'Conchalí'),
+(4, 'El Bosque'),
+(5, 'Estación Central'),
+(6, 'Huechuraba'),
+(7, 'Independencia'),
+(8, 'La Cisterna'),
+(9, 'La Florida'),
+(10, 'La Granja'),
+(11, 'La Pintana'),
+(12, 'La Reina'),
+(13, 'Las Condes'),
+(14, 'Lo Barnechea'),
+(15, 'Lo Espejo'),
+(16, 'Lo Prado'),
+(17, 'Macul'),
+(18, 'Maipú'),
+(19, 'Ñuñoa'),
+(20, 'Padre Hurtado'),
+(21, 'Pedro Aguirre Cerda'),
+(22, 'Peñalolén'),
+(23, 'Pirque'),
+(24, 'Providencia'),
+(25, 'Pudahuel'),
+(26, 'Puente Alto'),
+(27, 'Quilicura'),
+(28, 'Quinta Normal'),
+(29, 'Recoleta'),
+(30, 'Renca'),
+(31, 'San Bernardo'),
+(32, 'San Joaquín'),
+(33, 'San José De Maipo'),
+(34, 'San Miguel'),
+(35, 'San Ramón'),
+(36, 'Santiago'),
+(37, 'Vitacura');
+
+--
+-- Volcado de datos para la tabla `contrato`
+--
+
+INSERT INTO `contrato` (`id`, `rut_empleado`, `fecha_inicio`, `fecha_termino`, `id_tipo_contrato`, `id_prevision`) VALUES
+(1, 6556889, '2011-12-12', NULL, 3, 7),
+(2, 6862553, '2011-12-12', NULL, 1, 4),
+(3, 18463462, '2011-12-12', NULL, 4, 5),
+(4, 16624533, '2012-01-02', NULL, 4, 4),
+(5, 17286878, '2012-01-02', NULL, 4, 5),
+(6, 7986524, '2011-12-12', NULL, 1, 4),
+(7, 9388248, '2012-01-02', NULL, 2, 2),
+(8, 6874719, '2011-12-12', NULL, 1, 2),
+(9, 12633285, '2012-01-02', NULL, 2, 4),
+(10, 17837805, '2012-05-14', NULL, 4, 7),
+(11, 18122356, '2011-12-12', NULL, 4, 5),
+(12, 18365594, '2012-06-01', NULL, 4, 4),
+(13, 11627747, '2012-02-01', NULL, 4, 7),
+(14, 5200634, '2011-12-12', NULL, 3, 8),
+(15, 6553524, '2011-12-12', NULL, 2, 1),
+(16, 16791290, '2011-12-12', NULL, 2, 2),
+(17, 18909044, '2012-05-02', NULL, 4, 5),
+(18, 18221887, '2012-04-12', NULL, 4, 4),
+(19, 5894235, '2011-12-12', NULL, 1, 7),
+(20, 18530076, '2012-04-12', NULL, 4, 2);
+
+--
+-- Volcado de datos para la tabla `descuento_prevision`
+--
+
+INSERT INTO `descuento_prevision` (`id`, `id_prevision`, `fecha_periodo`, `descuento`) VALUES
+(1, 1, '2012-06-01', 0.0000),
+(2, 2, '2012-06-30', 0.1144),
+(3, 3, '2012-06-30', 0.1148),
+(4, 4, '2012-05-30', 0.1136),
+(5, 4, '2012-06-30', 0.1127),
+(6, 5, '2012-07-30', 0.1114),
+(8, 6, '2012-06-30', 0.1236),
+(9, 7, '2012-06-01', 0.1154),
+(10, 8, '2012-06-01', 0.2184);
+
+--
+-- Volcado de datos para la tabla `empleado`
+--
+
+INSERT INTO `empleado` (`rut`, `apellidos`, `nombres`, `fecha_nacimiento`, `direccion`, `fono`, `id_comuna`) VALUES
+(5200634, 'Jofré Maldonado', 'José Nelson Williams', '1947-08-07', 'Pedro Onca 768 Villa Polaris', '96535807', 31),
+(5894235, 'Soto Perez', 'Jorge Segundo', '1951-01-10', 'Pasaje Los Araucanos 840', '71098565', 18),
+(6553524, 'Muñóz Alegría', 'Víctor', '1954-06-15', 'Diego De Almagro 1352 Villa Tijerales', '76928281', 27),
+(6556889, 'Alarcón Muñoz', 'Carlos Cirilio', '1955-08-29', 'Pasaje San Ignacio 8095 Jardín Alto', '76266278', 9),
+(6862553, 'Arias Fortunatti', 'Luis Juan De Dios', '1952-02-20', 'Río Claro 3914 Villa Las Cumbres', '71098531', 26),
+(6874719, 'Castillo Araya', 'Hugo', '1955-10-12', 'Pantaleón Velez Silva 1292', '71098566', 7),
+(7986524, 'Caro Torres', 'José Alejandro', '1960-04-23', 'Pasaje Santa Carolina 1600', '71098567', 18),
+(9388248, 'Caro Torres', 'David Alberto', '1964-07-03', 'Quebec 8047', '96162139', 16),
+(11627747, 'Jofré Costa', 'Marco Antonio', '1970-05-06', 'Pedro Onca 768 Villa Polaris', '96535807', 31),
+(12633285, 'Concha Hernández', 'José Otalicio', '1974-06-30', 'Nueva San Martín 1248', NULL, 18),
+(16624533, 'Arriaran Toledo', 'Felipe Alberto', '1987-07-03', 'Pasaje Winipeg 3124 Villa Canadá', '82137840', 18),
+(16791290, 'Recabarren Mancilla', 'Oriel Iván', '1988-08-16', 'Pasaje Santa Carolina 1600', '84259109', 18),
+(17286878, 'Caro Luna', 'Nicolás Dario', '1990-03-28', 'Mallendo 645 Villa Arturo Prat', '95858443', 16),
+(17837805, 'Díaz Álvarez', 'Claudio Patricio', '1991-06-11', 'Talcón 5622', '73680703', 5),
+(18122356, 'García Soto', 'José Cristobal', '1992-08-17', 'Pasaje Canadá 3075 Villa Mancai', '76663737', 18),
+(18221887, 'Rubio Reyes', 'José Daniel', '1992-09-03', 'Los Coihues 11008', '75976648', 4),
+(18365594, 'Gonzalez Ulloa', 'Víctor Alfonso', '1993-02-04', 'Alcalde Hernán Prieto 29B', '68541266', 23),
+(18463462, 'Arias Quiroga', 'Piero Sebastián', '1993-08-09', 'Río Claro 3914 Villa Las Cumbres', '84809852', 26),
+(18530076, 'Zúñiga Acosta', 'Mauricio Esteban', '1992-12-12', 'Sargento Menadier 2563 Depto. 108', '92074350', 26),
+(18909044, 'Romero Gonzalez', 'Samuel Ignacio', '1995-02-02', 'Vicente Yañez Pinzon 3677 Barrio Las Rosas', '78518942', 18);
+
+--
+-- Volcado de datos para la tabla `pacto_salud`
+--
+
+INSERT INTO `pacto_salud` (`id`, `id_sistema_salud`, `id_contrato`, `fecha_periodo`, `pacto`) VALUES
+(1, 1, 1, '2012-06-30', NULL),
+(2, 2, 2, '2012-06-30', 4.973),
+(3, 2, 7, '2012-06-30', 2.804),
+(4, 2, 8, '2012-06-30', 4.082),
+(5, 2, 16, '2012-06-30', 2.238),
+(6, 2, 19, '2012-06-30', 4.852),
+(7, 4, 9, '2012-06-30', 6.064),
+(40, 1, 3, '2012-06-30', NULL),
+(41, 1, 4, '2012-06-30', NULL),
+(42, 1, 5, '2012-06-30', NULL),
+(43, 1, 6, '2012-06-30', NULL),
+(44, 1, 10, '2012-06-30', NULL),
+(45, 1, 11, '2012-06-30', NULL),
+(46, 1, 12, '2012-06-30', NULL),
+(47, 1, 13, '2012-06-30', NULL),
+(48, 1, 14, '2012-06-30', NULL),
+(49, 1, 15, '2012-06-30', NULL),
+(50, 1, 17, '2012-06-30', NULL),
+(51, 1, 18, '2012-06-30', NULL),
+(52, 1, 20, '2012-06-30', NULL);
+
+--
+-- Volcado de datos para la tabla `parametro_externo`
+--
+
+INSERT INTO `parametro_externo` (`id`, `codigo`, `fecha_vigencia`, `valor`) VALUES
+(1, 'VALOR_UF', '2012-06-30', 22627.37),
+(2, 'VALOR_UF', '2012-05-31', 22620.8),
+(3, 'VALOR_UF', '2012-04-30', 22591.21),
+(4, 'VALOR_UF', '2012-03-31', 22533.51),
+(5, 'VALOR_UF', '2012-02-29', 22462.79),
+(6, 'VALOR_UF', '2012-01-31', 22408.36),
+(7, 'VALOR_UF', '2011-12-31', 22294.03),
+(8, 'FACTOR_GRATIFICACION', '2012-06-30', 4.75),
+(9, 'DESCUENTO_SALUD', '2012-06-30', 0.07),
+(10, 'FACTOR_HORA_EXTRA', '2012-06-30', 0.0077777),
+(11, 'SUELDO_MINIMO', '2012-06-01', 182000);
+
+--
+-- Volcado de datos para la tabla `prevision`
+--
+
+INSERT INTO `prevision` (`id`, `nombre`) VALUES
+(1, 'Jubilado'),
+(2, 'Capital'),
+(3, 'Cuprum'),
+(4, 'Habitat'),
+(5, 'Modelo'),
+(6, 'Planvital'),
+(7, 'Provida'),
+(8, 'IPS (EX-INP)');
+
+--
+-- Volcado de datos para la tabla `registro_mensual`
+--
+
+INSERT INTO `registro_mensual` (`id`, `id_contrato`, `cantidad_horas_extras`, `monto_anticipo`, `bono_movilizacion`, `bono_colacion`, `bono_produccion`, `fecha_periodo`) VALUES
+(1, 5, 5.50, 100000, 15000, 15000, 10000, '2012-07-01'),
+(2, 11, 15.00, 100000, 10000, 10000, NULL, '2012-07-01'),
+(3, 10, 15.00, 10000, 10000, 1000, NULL, '2012-07-01'),
+(4, 8, 10.50, 100000, 10000, 15000, NULL, '2012-07-01'),
+(7, 4, 15.00, 0, NULL, NULL, NULL, '2012-07-01'),
+(8, 12, 15.00, 15000, NULL, NULL, NULL, '2012-07-01'),
+(9, 13, 15.00, 10000, NULL, NULL, NULL, '2012-07-01'),
+(13, 16, 15.00, 10000, 50000, 50000, NULL, '2012-07-01'),
+(14, 20, 15.00, 20000, NULL, NULL, NULL, '2012-07-01'),
+(17, 1, 15.00, 15000, NULL, 15000, 100000, '2012-07-01'),
+(18, 17, 15.00, 15000, NULL, NULL, NULL, '2012-07-01'),
+(19, 2, NULL, NULL, 50000, 50000, 150000, '2012-07-01'),
+(20, 2, NULL, NULL, 50000, 50000, 150000, '2012-06-01'),
+(24, 2, NULL, NULL, 50000, 50000, 100000, '2012-05-01'),
+(25, 3, 33.50, 60000, 8000, NULL, NULL, '2012-06-01'),
+(26, 3, 8.00, 120000, 15000, NULL, NULL, '2012-05-01'),
+(27, 6, NULL, 300000, 50000, 50000, 150000, '2012-06-01'),
+(30, 6, NULL, 300000, 50000, 50000, 100000, '2012-05-01'),
+(31, 19, NULL, 150000, 50000, 50000, 150000, '2012-06-01'),
+(32, 19, NULL, 150000, 50000, 50000, 100000, '2012-05-01'),
+(33, 8, NULL, NULL, 50000, 50000, 150000, '2012-06-01'),
+(34, 8, NULL, NULL, 50000, 50000, 100000, '2012-05-01'),
+(40, 17, 58.00, 80000, 15000, NULL, NULL, '2012-06-01'),
+(41, 17, 21.00, 80000, 15000, NULL, NULL, '2012-05-01'),
+(42, 5, 33.50, 40000, 11500, NULL, NULL, '2012-06-01'),
+(43, 5, NULL, 90000, 15000, NULL, NULL, '2012-05-01'),
+(44, 13, 48.00, 130000, 14500, NULL, NULL, '2012-06-01'),
+(45, 13, 17.00, 130000, 15000, NULL, NULL, '2012-05-01'),
+(46, 9, 65.00, 120000, 15000, NULL, NULL, '2012-06-01'),
+(47, 9, 28.00, 100000, 15000, NULL, NULL, '2012-05-01'),
+(48, 14, 49.00, NULL, 15000, NULL, NULL, '2012-06-01'),
+(49, 14, 28.00, NULL, 15000, NULL, NULL, '2012-05-01'),
+(50, 12, 59.50, 100000, 15000, NULL, NULL, '2012-06-01'),
+(51, 10, 59.50, 100000, 15000, NULL, NULL, '2012-06-01'),
+(52, 10, 17.00, NULL, 8500, NULL, NULL, '2012-05-01'),
+(53, 7, 40.50, 200000, 15000, NULL, NULL, '2012-06-01'),
+(54, 7, 21.00, 200000, 15000, NULL, NULL, '2012-05-01'),
+(55, 15, 40.00, 25000, 15000, NULL, NULL, '2012-06-01'),
+(56, 15, 21.00, 250000, 15000, NULL, NULL, '2012-05-01'),
+(57, 11, 54.00, 120000, 14500, NULL, NULL, '2012-06-01'),
+(58, 11, 9.00, 120000, 15000, NULL, NULL, '2012-05-01'),
+(59, 18, 60.00, 105000, 15000, NULL, NULL, '2012-06-01'),
+(60, 18, 26.00, 100000, 15000, NULL, NULL, '2012-05-01'),
+(61, 20, 42.50, 100000, 15000, NULL, NULL, '2012-06-01'),
+(62, 20, 6.00, 100000, 15000, NULL, NULL, '2012-05-01'),
+(63, 4, 52.50, 120000, 15000, NULL, NULL, '2012-06-01'),
+(64, 4, 17.00, 30000, 15000, NULL, NULL, '2012-05-01'),
+(65, 1, 66.50, 100000, 15000, NULL, NULL, '2012-06-01'),
+(66, 1, 32.50, 125000, 15000, NULL, NULL, '2012-05-01');
+
+--
+-- Volcado de datos para la tabla `renta_contrato`
+--
+
+INSERT INTO `renta_contrato` (`id`, `id_tipo_contrato`, `fecha_periodo`, `renta_bruta`, `gratificacion`) VALUES
+(1, 1, '2012-03-30', 182000, 0),
+(2, 1, '2012-04-30', 600000, 0),
+(3, 2, '2012-06-30', 413958, 1),
+(4, 3, '2012-06-30', 341958, 1),
+(5, 4, '2012-06-30', 259200, 1);
+
+--
+-- Volcado de datos para la tabla `sistema_salud`
+--
+
+INSERT INTO `sistema_salud` (`id`, `nombre`, `tiene_pacto`) VALUES
+(1, 'Fonasa', 0),
+(2, 'Consalud', 1),
+(3, 'Banmédica', 1),
+(4, 'Cruz Blanca', 1),
+(5, 'Colmena Golden Cross', 1),
+(6, 'Masvida', 1);
+
+--
+-- Volcado de datos para la tabla `tipo_contrato`
+--
+
+INSERT INTO `tipo_contrato` (`id`, `cargo`) VALUES
+(1, 'Socio'),
+(2, 'Primera'),
+(3, 'Segunda'),
+(4, 'Ayudante');
+SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
