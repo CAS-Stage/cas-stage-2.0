33837ef43c03f3182c4f6a57b976f493d9ffe7cc
Alberto Castillo Veloso <pikalbert@gmail.com>
2012-12-04 19:24:57 -0300

    Footer modificado
    Vista de impresión corregida.

--- application/cache/smarty/compiled/930b36aafb09185615ce4f3b478d2488c34c5b3d.file.body_footer.tpl.php
+++ application/cache/smarty/compiled/930b36aafb09185615ce4f3b478d2488c34c5b3d.file.body_footer.tpl.php
@@ -1,25 +1,25 @@
-<?php /* Smarty version Smarty-3.0.9, created on 2012-12-04 18:05:44
+<?php /* Smarty version Smarty-3.0.9, created on 2012-12-04 23:19:00
          compiled from "application/views/includes/body_footer.tpl" */ ?>
-<?php /*%%SmartyHeaderCode:257509eef3538cac3-38628544%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+<?php /*%%SmartyHeaderCode:2392950be76d4eb0da9-90729929%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
   array (
     '930b36aafb09185615ce4f3b478d2488c34c5b3d' => 
     array (
       0 => 'application/views/includes/body_footer.tpl',
-      1 => 1354222189,
+      1 => 1354659516,
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '257509eef3538cac3-38628544',
+  'nocache_hash' => '2392950be76d4eb0da9-90729929',
   'function' => 
   array (
   ),
   'has_nocache_code' => false,
 )); /*/%%SmartyHeaderCode%%*/?>
-        <footer>
+<footer>
             <p>Sistema CAS Stage</p>
             <p>Desarrollado por: Matías Astorga Donoso &bull; Alberto Castillo Veloso &bull; Juan Cid Carreño &bull; Jorge Guitard Torres</p>
+            <p>Soporte Técnico: <a href="mailto:soporte.cas.stage@gmail.com">soporte.cas.stage@gmail.com </a></p>
             <p>Uso exclusivo de la empresa CAS Stage Ltda.</p>
-            <p>Soporte Técnico: <a href="mailto:soporte.cas.stage@gmail.com">soporte.cas.stage@gmail.com</p>
         </footer>
